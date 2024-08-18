66557861b6a090a56c8975143b7fde9807f238d1
akoksrud <andreas@koksrud.no>
2024-08-18 17:26:30 +0000

    Updated Lab12 solution

--- solutions/Lab12_Python-get-ap-table/get-ap-table.py
+++ solutions/Lab12_Python-get-ap-table/get-ap-table.py
@@ -4,9 +4,10 @@ import json
 import openpyxl
 import getpass
 
-url = "https://192.168.10.10/restconf/data/Cisco-IOS-XE-wireless-access-point-oper:access-point-oper-data/capwap-data?fields=wtp-mac;wtp-ip;name;ap-state;device-detail/static-info/board-data/wtp-serial-num;device-detail/static-info/board-data/wtp-enet-mac;device-detail/static-info/ap-models/model;tag-info/tag-source;tag-info/policy-tag-info;tag-info/site-tag;tag-info/rf-tag;tag-info/filter-info/filter-name;ap-time-info/boot-time;ap-time-info/join-time"
+wlc = input("Enter WLC IP: ")
 user = input("Enter user: ")
 password = getpass.getpass("Enter password: ")
+url = f"https://{wlc}/restconf/data/Cisco-IOS-XE-wireless-access-point-oper:access-point-oper-data/capwap-data?fields=wtp-mac;wtp-ip;name;ap-state;device-detail/static-info/board-data/wtp-serial-num;device-detail/static-info/board-data/wtp-enet-mac;device-detail/static-info/ap-models/model;tag-info/tag-source;tag-info/policy-tag-info;tag-info/site-tag;tag-info/rf-tag;tag-info/filter-info/filter-name;ap-time-info/boot-time;ap-time-info/join-time"
 
 payload = {}
 headers = {
